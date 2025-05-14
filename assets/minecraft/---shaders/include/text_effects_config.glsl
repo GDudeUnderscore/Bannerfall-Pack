fc05c2b7ade998501d549d9bb344d80a93452ee9
GDude_ <GQM3443@gmail.com>
2025-05-13 21:09:43 -0400

    pack

---
+++ assets/minecraft/---shaders/include/text_effects_config.glsl
@@ -0,0 +1,17 @@
+
+TEXT_EFFECT(15, 255, 255) {
+    apply_waving_movement();
+    override_text_color(rgb(0, 164, 137));
+    override_shadow_color(rgb(0, 50, 52));
+    apply_shimmer();
+    apply_gradient(rgb(0, 164, 137), rgb(0, 240, 200));
+    apply_outline(rgb(0, 90, 91));
+}
+
+TEXT_EFFECT(89, 255, 106) {
+    apply_shimmer();
+}
+
+TEXT_EFFECT(255, 71, 81) {
+    apply_shaking_movement();
+}
